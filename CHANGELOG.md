1a32ef94f9669f990909fd40c4069f1a438c19fc
Owais Lone <owais@lone.pw>
2021-03-10 01:07:39 +0530

    Extended startTracing() options.
    startTracing options now accept the following parameters:
    
        tracerConfig: JS object that is merged with default config and
        passed on to the tracer provider.
    
        spanExporterFactory: a function that accepts the options passed to
        startTracing and return a new instance of a SpanExporter.
    
        spanProcessorFactory: a function that accepts the options passed to
        startTracing and return one or more instances of a SpanProcessor.
    
    These three options allow users to customize the tracing pipeline to a
    huge degree.

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+# Changelog
+
+## Unreleased
+
+- `startTracing()` options now accepts a `tracerConfig` option which is
+  merged with the default tracer config and passed on to the tracer provider.
+
+- Added `spanExporterFactory` option to `startTracing()` options.
+  `spanExporterFactory` receives a processed `Options` instance and
+  returns a new instance of `Exporter`.
+
+- Replaced `spanProcessor` option with `spanProcessorFactory`.
+  `startTracing()` options now accepts a `spanProcessorFactory` function. The function
+  accept a processed `Options` instance and returns a `SpanProcessor`
+  instance or an array of `SpanProcessor` instances. It can be used to configure tracing
+  with custom Span Processor. If it returns multiple span processors, all of them will
+  be used.
---
+++ test/options.test.ts
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2021 Splunk Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { InstrumentationBase } from '@opentelemetry/instrumentation';
+import { Resource } from '@opentelemetry/resources';
+import {
+  SimpleSpanProcessor,
+  SpanExporter,
+  SpanProcessor,
+  InMemorySpanExporter,
+} from '@opentelemetry/tracing';
+import * as assert from 'assert';
+
+import {
+  _setDefaultOptions,
+  defaultSpanProcessorFactory,
+  defaultSpanExporterFactory,
+  Options,
+} from '../src/options';
+
+describe('options', () => {
+  it('verify default options', () => {
+    const options = _setDefaultOptions();
+    assert.deepStrictEqual(options, {
+      endpoint: 'http://localhost:9080/v1/trace',
+      serviceName: 'unnamed-node-service',
+      accessToken: '',
+      maxAttrLength: 1200,
+      instrumentations: [],
+      tracerConfig: {
+        resource: new Resource({}),
+      },
+      spanExporterFactory: defaultSpanExporterFactory,
+      spanProcessorFactory: defaultSpanProcessorFactory,
+    });
+  });
+
+  it('verify custom options', () => {
+    const testInstrumentation = new TestInstrumentation('inst', '1.0');
+    const idGenerator = new TestIdGenerator();
+
+    const options = _setDefaultOptions({
+      endpoint: 'custom-endpoint',
+      serviceName: 'custom-service-name',
+      accessToken: 'custom-access-token',
+      maxAttrLength: 4000,
+      instrumentations: [testInstrumentation],
+      tracerConfig: {
+        resource: new Resource({
+          attr1: 'value',
+        }),
+        idGenerator: idGenerator,
+      },
+      spanExporterFactory: testSpanExporterFactory,
+      spanProcessorFactory: testSpanProcessorFactory,
+    });
+
+    assert.deepStrictEqual(options, {
+      endpoint: 'custom-endpoint',
+      serviceName: 'custom-service-name',
+      accessToken: 'custom-access-token',
+      maxAttrLength: 4000,
+      instrumentations: [testInstrumentation],
+      tracerConfig: {
+        resource: new Resource({ attr1: 'value' }),
+        idGenerator: idGenerator,
+      },
+      spanExporterFactory: testSpanExporterFactory,
+      spanProcessorFactory: testSpanProcessorFactory,
+    });
+  });
+});
+
+class TestInstrumentation extends InstrumentationBase {
+  init() {}
+}
+
+class TestIdGenerator {
+  generateTraceId(): string {
+    return '';
+  }
+
+  generateSpanId(): string {
+    return '';
+  }
+}
+
+function testSpanExporterFactory(): SpanExporter {
+  return new InMemorySpanExporter();
+}
+
+function testSpanProcessorFactory(options: Options): SpanProcessor {
+  return new SimpleSpanProcessor(options.spanExporterFactory(options));
+}
