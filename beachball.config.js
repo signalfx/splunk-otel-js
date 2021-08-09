e505c0b388de3bf99c0236bab8e525babeb2a840
Jakub Malinowski <jakubmal+github@gmail.com>
2021-08-09 14:23:42 +0200

    ci: automate github release

--- beachball.config.js
+++ beachball.config.js
@@ -7,4 +7,7 @@ module.exports = {
   // intended to be run via CI, so remote = local
   fetch: false,
   push: false,
+
+  // generate the changelog and send a PR, and then it's published
+  publish: false,
 };
