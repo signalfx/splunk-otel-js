ed37d9926718092a6366d9f758d3bd4bc7f3c893
Siim Kallas <siimkallas@gmail.com>
2021-11-10 18:05:28 +0200

    debug artifact

--- scripts/fetch-release-artifact.js
+++ scripts/fetch-release-artifact.js
@@ -6,21 +6,44 @@ const { version } = require('../package.json');
 const { Console } = require('console');
 
 async function getBuildArtifact() {
-  //const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-  const octokit = new Octokit();
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+  //const octokit = new Octokit();
 
   const owner = process.env.GITHUB_OWNER ?? 'signalfx';
   const repo = process.env.GITHUB_REPO ?? 'splunk-otel-js';
 
 
-  const workflows = octokit.rest.actions.listWorkflowRunsForRepo({
+  const {data: workflows} = await octokit.rest.actions.listWorkflowRunsForRepo({
     owner,
     repo,
     branch: process.env.CI_COMMIT_BRANCH ?? 'main',
   });
 
+  const runs = workflows.workflow_runs;
+
   console.log('workflows');
-  console.log(workflows);
+  console.log(runs);
+
+  const run = runs.find(wf => wf.head_sha === process.env.CI_COMMIT_SHA);
+
+  const {data: artifacts} = await octokit.rest.actions.listWorkflowRunArtifacts({
+    owner,
+    repo,
+    run_id: run.id,
+  });
+
+  const tgzName = `splunk-otel-${version}.tgz`
+  const packageArtifact = artifacts.artifacts.find(artifact => artifact.name === tgzName);
+
+  console.log(packageArtifact);
+
+  const dlArtifact = await octokit.rest.actions.downloadArtifact({
+    owner,
+    repo,
+    artifact_id: packageArtifact.id
+  });
+
+  console.log(dlArtifact);
 }
 
 getBuildArtifact().catch(e => {
