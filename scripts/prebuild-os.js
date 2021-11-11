ddacc1ed501b383a1a449c96fc8178a405643a6f
Siim Kallas <skallas@splunk.com>
2021-11-11 14:40:22 +0200

    upgrade prebuildify, attempt node 17 builds

--- scripts/prebuild-os.js
+++ scripts/prebuild-os.js
@@ -3,7 +3,7 @@ const prebuildify = require('prebuildify');
 let targets = process.argv.slice(2);
 
 if (targets.length == 0) {
-  targets = ['8.5.0', '9.0.0', '10.0.0', '11.0.0', '12.0.0', '13.0.0', '14.0.0', '15.0.0', '16.0.0'];
+  targets = ['8.5.0', '9.0.0', '10.0.0', '11.0.0', '12.0.0', '13.0.0', '14.0.0', '15.0.0', '16.0.0', '17.0.1'];
 }
 
 prebuildify({
