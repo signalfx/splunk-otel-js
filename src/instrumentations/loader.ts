595ef2eb3ec4474ad339a337488a93ff1d8d739a
Rauno Viskus <rauno56@gmail.com>
2022-05-12 15:05:47 +0300

    feat: make loading instrumentations npm-link-safe

--- src/instrumentations/loader.ts
+++ src/instrumentations/loader.ts
@@ -16,19 +16,36 @@
 
 import { diag } from '@opentelemetry/api';
 import { Instrumentation } from '@opentelemetry/instrumentation';
+import { createRequire } from 'module';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type InstrumentationConstructor = { new (config?: any): Instrumentation };
+
+const linkSafeRequire = createRequire(require?.main?.filename || __filename);
 
 export function load(
   module: string,
   instrumentation: string
 ): InstrumentationConstructor | null {
   try {
+    const loadedModule = linkSafeRequire(module);
+
+    if (loadedModule[instrumentation]) {
+      return loadedModule[instrumentation]
+    }
+  } catch {
+    // Ignoring errors and attempting the common case
+  }
+
+  try {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     return require(module)[instrumentation];
-  } catch (e) {
-    diag.debug('cannot find instrumentation package: ' + instrumentation);
+  } catch (e: any) {
+    if (e.code === 'MODULE_NOT_FOUND') {
+      diag.debug('cannot find instrumentation package:', module);
+    } else {
+      diag.debug('Errored loading instrumentation:', e.message, e.code);
+    }
   }
   return null;
 }
