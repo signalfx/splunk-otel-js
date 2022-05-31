650bacd9836b5455a562e6b14e1562c8434e5742
Rauno Viskus <rauno56@gmail.com>
2022-05-31 11:34:37 +0300

    style: add an empty line after the header

--- test/utils.ts
+++ test/utils.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 const isConfigVarEntry = key => {
   const lowercased = key.toLowerCase();
   return (
