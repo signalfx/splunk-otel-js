2df2a1e4ae1d36c7f02a2d79f3210fd70e6db52b
Rauno Viskus <Rauno56@users.noreply.github.com>
2022-05-31 16:07:36 +0300

    feat: throw when `startTracing` is called twice (#485)
    * test: check if server was initialized before closing it
    
    * feat: throw when `startTracing` is called twice
    
    * test: stop tracing in uri parameter capturing tests
    
    * style: add an empty line after the header
    
    * Change files
    
    * docs: fix typo for `startTracing`
    
    * style: fix lint

--- src/tracing/index.ts
+++ src/tracing/index.ts
@@ -14,24 +14,43 @@
  * limitations under the License.
  */
 
+import { strict as assert } from 'assert';
+import { gte } from 'semver';
+
 import { context, propagation, trace } from '@opentelemetry/api';
 import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
-
-import { configureHttpInstrumentation } from '../instrumentations/http';
-import { configureLogInjection } from '../instrumentations/logging';
-import { Options, _setDefaultOptions } from './options';
-import { gte } from 'semver';
 import {
   AsyncHooksContextManager,
   AsyncLocalStorageContextManager,
 } from '@opentelemetry/context-async-hooks';
+
+import { configureHttpInstrumentation } from '../instrumentations/http';
+import { configureLogInjection } from '../instrumentations/logging';
+import { Options, _setDefaultOptions } from './options';
 import { configureRedisInstrumentation } from '../instrumentations/redis';
+import { parseEnvBooleanString } from '../utils';
+
+/**
+ * We disallow calling `startTracing` twice because:
+ * 1. This is very rarely the user intention;
+ * 2. Causes unexpected applied configuration to OTel libs;
+ * 3. There's no way to reliably clean up before applying new configuration.
+ * However, having a mechanism to allow that in tests is useful even if it
+ * leaks and is inperfect in terms of the end result.
+ */
+const allowDoubleStart = parseEnvBooleanString(
+  process.env.TEST_ALLOW_DOUBLE_START
+);
+let isStarted = false;
 
 let unregisterInstrumentations: (() => void) | null = null;
 
 export { Options as TracingOptions };
 export function startTracing(opts: Partial<Options> = {}): boolean {
+  assert(!isStarted || allowDoubleStart, 'Splunk APM already started');
+  isStarted = true;
+
   const options = _setDefaultOptions(opts);
 
   // propagator
