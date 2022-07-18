22c970a44100ac0f4d9a5b19f9ea56f81c6e1a15
Rauno Viskus <rauno56@gmail.com>
2022-07-18 21:23:18 +0300

    feat: explicitly start and stop profiling in the example

--- examples/profiling/index.js
+++ examples/profiling/index.js
@@ -1,3 +1,4 @@
+const { start, stop } = require('@splunk/otel');
 const { diag, DiagConsoleLogger, DiagLogLevel, trace, SpanStatusCode, context } = require('@opentelemetry/api');
 
 // If OTEL_LOG_LEVEL env var is set, configure logger
@@ -5,24 +6,39 @@ if (process.env.OTEL_LOG_LEVEL) {
   diag.setLogger(new DiagConsoleLogger(), DiagLogLevel[process.env.OTEL_LOG_LEVEL]);
 }
 
+// Profiling is still experimental and have to be enabled explicitly
+start({
+	profiling: {
+		callstackInterval: 50,
+		collectionDuration: 1_000,
+	},
+});
+
 const doWork = () => {
 	console.log('before work');
+	const start = Date.now();
 	while (Date.now() - start < 2500) {}
 	console.log('after work');
 };
 
-const start = Date.now();
-const tracer = trace.getTracer('splunk-otel-example-profiling');
-const span = tracer.startSpan('main');
-const spanContext = trace.setSpan(context.active(), span);
+// This has to be here because of starting profiling asyncronously.
+// If we didn't we'd get start and collect commands too close together and collect would return no stacktraces
+setTimeout(() => {
+	const tracer = trace.getTracer('splunk-otel-example-profiling');
+	const span = tracer.startSpan('main');
+	const spanContext = trace.setSpan(context.active(), span);
 
-// Span and Trace IDs are attached to the profiling samples
-context.with(spanContext, doWork);
+	// Span and Trace IDs are attached to the profiling samples
+	context.with(spanContext, doWork);
 
-console.log('done!');
-span.end();
+	console.log('done!');
+	span.end();
+
+	// Stop profiling to flush the collected samples
+	stop();
+}, 100);
 
 setTimeout(() => {
-	// wait for the spans to be flushed
-	console.log('Profiling data exported');
-}, 5000);
+	// Stop profiling to flush the collected samples
+	stop();
+}, 3000);
