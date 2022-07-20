bac1cbaaef0c67db65b6dbf363194f1ef0a0c0e1
Rauno Viskus <rauno56@gmail.com>
2022-07-20 17:33:38 +0300

    style: intent with spaces

--- examples/profiling/index.js
+++ examples/profiling/index.js
@@ -8,31 +8,31 @@ if (process.env.OTEL_LOG_LEVEL) {
 
 // Profiling is still experimental and has to be enabled explicitly
 start({
-	profiling: {
-		callstackInterval: 100,
-		collectionDuration: 1_000,
-	},
+  profiling: {
+    callstackInterval: 100,
+    collectionDuration: 1_000,
+  },
 });
 
 const doWork = () => {
-	const start = Date.now();
-	while (Date.now() - start < 2000) {}
+  const start = Date.now();
+  while (Date.now() - start < 2000) {}
 };
 
 // setTimeout has to be here because profiling is currently started asyncronously to avoid blocking the runtime.
 // If we didn't we'd run stop before the profiling has started in the background.
 setTimeout(() => {
-	const tracer = trace.getTracer('splunk-otel-example-profiling');
-	const span = tracer.startSpan('main');
-	const spanContext = trace.setSpan(context.active(), span);
+  const tracer = trace.getTracer('splunk-otel-example-profiling');
+  const span = tracer.startSpan('main');
+  const spanContext = trace.setSpan(context.active(), span);
 
-	console.log('starting spinning');
-	// Span and Trace IDs are attached to the profiling samples
-	context.with(spanContext, doWork);
+  console.log('starting spinning');
+  // Span and Trace IDs are attached to the profiling samples
+  context.with(spanContext, doWork);
 
-	console.log('done!');
-	span.end();
+  console.log('done!');
+  span.end();
 
-	// Stop profiling to flush the collected samples
-	stop();
+  // Stop profiling to flush the collected samples
+  stop();
 }, 10);
