7550b16b68e2acde4b0a514efcd7f81cc9edbea3
Rauno Viskus <rauno56@gmail.com>
2022-07-21 16:00:16 +0300

    refactor: rename the detector file

--- src/resource.ts
+++ src/resource.ts
@@ -18,6 +18,7 @@ import { diag } from '@opentelemetry/api';
 import { Resource } from '@opentelemetry/resources';
 
 import { distroDetector } from './detectors/DistroDetector';
+import { dockerCGroupV1Detector } from './detectors/DockerCGroupV1Detector.ts';
 import { envDetector } from './detectors/EnvDetector';
 import { hostDetector } from './detectors/HostDetector';
 import { osDetector } from './detectors/OSDetector';
@@ -25,6 +26,7 @@ import { processDetector } from './detectors/ProcessDetector';
 
 const detectors = [
   distroDetector,
+  dockerCGroupV1Detector,
   envDetector,
   hostDetector,
   osDetector,
