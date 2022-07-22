c62e19fb4c00528df109a12cd8215cf479265b89
Siim Kallas <siimkallas@gmail.com>
2022-07-22 10:57:57 +0300

    fix: add a workaround for periodic exporting metric reader blocking process exit

--- src/metrics/index.ts
+++ src/metrics/index.ts
@@ -157,6 +157,13 @@ export function startMetrics(opts: StartMetricsOptions = {}) {
 
   metricReaders.forEach(reader => {
     provider.addMetricReader(reader);
+
+    // HACK until https://github.com/open-telemetry/opentelemetry-js/pull/3106 is published
+    if (reader instanceof PeriodicExportingMetricReader) {
+      if (reader['_interval']) {
+        reader['_interval'].unref();
+      }
+    }
   });
 
   metrics.setGlobalMeterProvider(provider);
