65a428e51c71bd885a37fbb341d50db9590d85d7
Siim Kallas <siimkallas@gmail.com>
2022-08-24 13:11:14 +0300

    chore: add proto/profile.js to coverage ignore

--- src/profiling/proto/profile.js
+++ src/profiling/proto/profile.js
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 /*eslint-disable block-scoped-var, id-length, no-control-regex, no-magic-numbers, no-prototype-builtins, no-redeclare, no-shadow, no-var, sort-vars*/
 "use strict";
 
