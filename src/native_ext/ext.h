0d0dda16940aeb46a650ead216d1ae715ac6389c
Siim Kallas <skallas@splunk.com>
2022-09-09 14:53:13 +0300

    fix: aligned macro fix for older gcc

--- src/native_ext/ext.h
+++ src/native_ext/ext.h
@@ -1,6 +1,12 @@
 #pragma once
 
-#if defined(__GNUC__) || defined(__clang__)
+#if defined(__GNUC__)
+  #define GCC_VERSION (__GNUC__ * 10000 \
+                      + __GNUC_MINOR__ * 100 \
+                      + __GNUC_PATCHLEVEL__)
+#endif
+
+#if (defined(__GNUC__) && GCC_VERSION >= 40900) || defined(__clang__)
 #define SPLK_ASSUME_ALIGNED(n) __attribute__((assume_aligned(n)))
 #else
 #define SPLK_ASSUME_ALIGNED(n)
