6bb9e69c495f3fd4854feb021b0bb441cf6884cd
Siim Kallas <siimkallas@gmail.com>
2022-10-14 14:44:29 +0300

    chore: remove unused type

--- src/start.ts
+++ src/start.ts
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { InstrumentationOption } from '@opentelemetry/instrumentation';
-
 import { assertNoExtraneousProperties, parseEnvBooleanString } from './utils';
 import { startMetrics, MetricsOptions } from './metrics';
 import { startProfiling, ProfilingOptions } from './profiling';
