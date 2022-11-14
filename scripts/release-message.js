88a81dc7fa45e57b1fd35b96cff1d42c1c765355
Siim Kallas <siimkallas@gmail.com>
2022-11-14 15:43:49 +0200

    chore: get rid of beachball

--- scripts/release-message.js
+++ scripts/release-message.js
@@ -1,7 +1,22 @@
 const { version } = require('../package.json');
 const { dependencies } = require('../package-lock.json');
+const { readFileSync } = require('fs');
 
 exports.getReleaseMessage = () => {
+  const changelog = readFileSync('../CHANGELOG.md', { encoding: 'utf-8' });
+  const changeHeaderBegin = changelog.indexOf(`## ${version}`);
+
+  if (changeHeaderBegin === -1) {
+    throw new Error(`Unable to find version ${version} in release notes.`);
+  }
+
+  const changesBegin = changelog.indexOf('\n', changeHeaderBegin) + 1;
+
+  const prevVersionMatch = /^##\s*\d+\.\d+\.\d+.*$/gm.exec(changelog.substring(changesBegin));
+  const nextVersionBegin = prevVersionMatch ? prevVersionMatch.index : changelog.length
+
+  const versionChanges = changelog.substring(changesBegin, changesBegin + nextVersionBegin - 1);
+
   const otelApiVersion = dependencies['@opentelemetry/api'].version;
   const otelCoreVersion = dependencies['@opentelemetry/core'].version;
   const otelInstrumentationVersion = dependencies['@opentelemetry/instrumentation-http'].version;
@@ -12,6 +27,6 @@ exports.getReleaseMessage = () => {
     `| ${otelApiVersion} | ${otelCoreVersion} | ${otelInstrumentationVersion} |`,
     '',
     '## Changes',
-    '',
+    versionChanges,
   ].join('\n');
 };
