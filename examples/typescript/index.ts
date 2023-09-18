70b0fdef65d14d2795141277c05d1382f17451f8
Siim Kallas <siimkallas@gmail.com>
2023-09-18 23:29:13 +0300

    test: add typescript e2e test

---
+++ examples/typescript/index.ts
@@ -0,0 +1,34 @@
+import { start } from '@splunk/otel';
+import * as otel from '@opentelemetry/api';
+
+start({
+  serviceName: 'random-number-generator',
+  // Set up the OpenTelemetry metrics pipeline and start collecting runtime metrics.
+  metrics: {
+    runtimeMetricsEnabled: true,
+  }
+});
+
+// Load libraries after calling start()
+import * as express from 'express';
+
+const tracer = otel.trace.getTracer('rng-app');
+const meter = otel.metrics.getMeter('rng-app');
+const requestCounter = meter.createCounter('requests');
+
+const app = express();
+
+app.get("/", (req, res) => {
+  requestCounter.add(1);
+  const randomValue = tracer.startActiveSpan('calculate-random', (span) => {
+    const result = (Math.random() * 42) | 0;
+    span.setAttribute('random-result', result);
+    span.setAttribute('random-method', 'diceroll');
+    span.end();
+    return result;
+  });
+
+  res.json({ randomValue });
+});
+
+app.listen(process.env.PORT || 8080);
