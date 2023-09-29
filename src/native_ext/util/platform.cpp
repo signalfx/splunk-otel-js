342b235323f32f40e6ca61a7e4796a35454e9123
Siim Kallas <siimkallas@gmail.com>
2023-09-30 01:04:14 +0300

    fix: use correct header

--- src/native_ext/util/platform.cpp
+++ src/native_ext/util/platform.cpp
@@ -21,8 +21,7 @@ int64_t HrTime() { return uv_hrtime(); }
 #endif
 
 #ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
+#include <sysinfoapi.h>
 int64_t MicroSecondsSinceEpoch() {
   FILETIME ft;
   GetSystemTimePreciseAsFileTime(&ft);
@@ -31,7 +30,6 @@ int64_t MicroSecondsSinceEpoch() {
   t -= 116444736000000000LL;
   return t / 10LL;
 }
-#undef WIN32_LEAN_AND_MEAN
 #else
 #include <time.h>
 int64_t MicroSecondsSinceEpoch() {
