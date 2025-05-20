417222ed7e859a9ca1b0163c7798651f1e87fe82
Siim Kallas <siimkallas@gmail.com>
2025-05-20 21:22:04 +0300

    feat: experimental http instrumentation based on diagnostics channel (#1021)
    * init
    
    * tests
    
    * tests
    
    * improvements, linting
    
    * renaming
    
    * fix tests
    
    * disable mt check
    
    * skip dc tests for old nodejs versions
    
    * fix test skip
    
    * fix test version checking

---
+++ src/instrumentations/httpdc/semconv.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright Splunk Inc., The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+export const HTTP_ERROR_NAME = 'http.error_name' as const;
+export const HTTP_ERROR_MESSAGE = 'http.error_message' as const;
+export const HTTP_STATUS_TEXT = 'http.status_text' as const;
