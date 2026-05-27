82656c24386b72542fbeba30ac18270f38035f9e
Marten Hennoch <mhennoch@cisco.com>
2026-05-27 16:20:18 +0300

    vendor in mysql2

---
+++ src/instrumentations/external/mysql2/version.ts
@@ -0,0 +1,8 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Upstream generates this file during package publishing.
+export const PACKAGE_VERSION = '0.64.0';
+export const PACKAGE_NAME = '@opentelemetry/instrumentation-mysql2';
