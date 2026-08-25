a689555f4ab307acd4a2480ad4026c63075a7bc9
mhennoch <mhennoch@splunk.com>
2026-08-25 13:13:15 +0300

    Update mysql2

--- src/instrumentations/external/mysql2/version.ts
+++ src/instrumentations/external/mysql2/version.ts
@@ -4,5 +4,5 @@
  */
 
 // Upstream generates this file during package publishing.
-export const PACKAGE_VERSION = '0.64.0';
+export const PACKAGE_VERSION = '0.67.0';
 export const PACKAGE_NAME = '@opentelemetry/instrumentation-mysql2';
